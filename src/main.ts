9d9b762bd28206b5935cc949834091cb06dfdaa0
dartpilot0017 <pilot09923@gmail.com>
2024-11-30 14:14:41 +0100

    Swagger documentation create

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,8 @@
 /* eslint-disable prettier/prettier */
-// import { NestFactory } from '@nestjs/core';
-// import { AppModule } from './app.module';
-
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule);
-//   await app.listen(process.env.PORT ?? 3000);
-// }
-// bootstrap();
-
-// src/main.ts
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -25,6 +16,21 @@ async function bootstrap() {
     }),
   );
 
+  // Swagger configuration
+  const config = new DocumentBuilder()
+    .setTitle('Driveway Service API')  // Set the title of your API documentation
+    .setDescription('API documentation for the Driveway Service')  // Description
+    .setVersion('1.0')  // Version of your API
+    .addTag('auth')  // Optional: tag for authentication-related APIs
+    .build();
+
+  // Create Swagger document
+  const document = SwaggerModule.createDocument(app, config);
+
+  // Setup Swagger UI
+  SwaggerModule.setup('api', app, document);  // Swagger UI will be available at '/api'
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();

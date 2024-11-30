28e6201c571bb7bdc87b74da2a4aae93630a2aa5
dartpilot0017 <pilot09923@gmail.com>
2024-11-30 15:14:11 +0100

    Update jwt.strategy.ts

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,39 +1,17 @@
 /* eslint-disable prettier/prettier */
-// // src/auth/jwt.strategy.ts
-// import { Injectable } from '@nestjs/common';
-// import { PassportStrategy } from '@nestjs/passport';
-// import { ExtractJwt, Strategy } from 'passport-jwt';
-
-// @Injectable()
-// export class JwtStrategy extends PassportStrategy(Strategy) {
-//   constructor() {
-//     super({
-//       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-//       ignoreExpiration: false,
-//       secretOrKey: 'yourSecretKey', // Use an environment variable in production
-//     });
-//   }
-
-//   async validate(payload: any) {
-//     return { userId: payload.sub, email: payload.email };
-//   }
-// }
-
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, ExtractJwt } from 'passport-jwt';
 import { AuthService } from './auth.service';
-import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(
-    private authService: AuthService,
-    private configService: ConfigService
+    private authService: AuthService
   ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: configService.get('JWT_KEY'), // use your secret key or environment variable
+      secretOrKey: process.env.JWT_KEY, // use your secret key or environment variable
       ignoreExpiration: false,
     });
   }

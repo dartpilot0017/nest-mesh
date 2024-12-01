52a1bdb7ada729443763ab2b7e8a2722eb47dfe6
mybllog <chisomeleanora@gmail.com>
2024-12-01 06:56:17 +0100

    made changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,6 +14,7 @@ import { ReportsModule } from './reports/reports.module';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      envFilePath: '.env'
     }),
     DatabaseModule, // Add the DatabaseModule here
     AuthModule,
